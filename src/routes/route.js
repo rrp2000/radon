08937d7a2be83cb69bc6e3e4a869f45e7352754c
rrp2000 <ranjanpatel12391@gmail.com>
2022-06-03 13:57:05 +0530

    this commit is for submission of assignment 3/6/22

--- src/routes/route.js
+++ src/routes/route.js
@@ -8,107 +8,59 @@ router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
 
-//problem 1
-router.get('/movies',function(req , res){
-    let movies = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
-    res.send(movies);
-})
+//pritesh sir assignmnet 3/6/2022
 
-//problem 2 and 3
-router.get('/movies/:indexNumber',function(req , res){
-    let movies = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
-    index = req.params.indexNumber;
-    if(index<movies.length)
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ],
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ],
+       },
+   ]
+
+router.post('/players', function (req, res) {
+ 
+    let newPlayer = req.body;
+    let flag = true;
+    players.forEach(x => {
+        if(x.name === newPlayer.name)
+        {
+            flag = false;
+        }
+    })
+    if(flag == true)
     {
-        res.send(movies[index]);
+        players.push(newPlayer);
+        res.send(  { data: players , status: true }  )
     }
     else
     {
-        res.send("Please enter a valid index.")
+        res.send("A player already exists with this name. cant add.")
     }
     
 })
-
-// problem 4
-router.get('/films',function(req , res){
-    let films = [{
-        'id': 1,
-        'name': 'The Shining'},
-        {
-        'id': 2,
-        'name': 'Incendies'},
-        {
-        'id': 3,
-        'name': 'Rang de Basanti'},
-        {
-        'id': 4,
-        'name': 'Finding Nemo'}
-    ]
-    
-    res.send(films);
-
-})
-
-//problem 5
-router.get('/films/:filmId',function(req , res){
-    const films = [{
-        'id': 1,
-        'name': 'The Shining'},
-        {
-        'id': 2,
-        'name': 'Incendies'},
-        {
-        'id': 3,
-        'name': 'Rang de Basanti'},
-        {
-        'id': 4,
-        'name': 'Finding Nemo'}
-    ]
-
-    const getId = req.params.filmId;
-    if(getId<films.length+1)
-    {
-        res.send(films[getId-1])
-    }
-    else
-    {
-        res.send("No movie exists with the id "+getId)
-    }
-
-})
-
-router.get("/sol1", function (req, res){
-    let arr= [1,2,3,5,6,7]
-    let missingNumber =0;
-    let n = arr[arr.length-1];
-    let totalSum = (n*(n+1))/2;
-    let arrSum = 0;
-    arr.forEach(x => {
-        arrSum = arrSum + x;
-    })
-    missingNumber = totalSum - arrSum;
-    res.send({"data": missingNumber})
-}) 
-
-
-router.get("/sol2", function (req, res){
-    let arr= [33,34,35,37,38];
-    let missingNumber =0;
-    n = arr.length;
-    firstNum = arr[0];
-    lastNum = arr[arr.length-1];
-    let totalSum = (n+1)*((firstNum+lastNum)/2);
-    let arrSum = 0;
-    arr.forEach(x => {
-        arrSum = arrSum + x;
-    })
-    missingNumber = totalSum - arrSum;
-    res.send({"data": missingNumber})
-}) 
-
-
-
-
-
 
 module.exports = router;
