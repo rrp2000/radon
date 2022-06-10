83de0f32c2ef8d2216eab893410517f69c549323
rrp2000 <ranjanpatel12391@gmail.com>
2022-06-10 23:32:43 +0530

    commit for assignment submition

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,35 +1,5 @@
 const UserModel= require("../models/userModel")
 
-
-
-
-const basicCode= async function(req, res) {
-    let tokenDataInHeaders= req.headers.token
-    console.log(tokenDataInHeaders)
-
-    console.log( "HEADER DATA ABOVE")
-    console.log( "hey man, congrats you have reached the Handler")
-    res.send({ msg: "This is coming from controller (handler)"})
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const createUser= async function (req, res) {
     let data= req.body
     let savedData= await UserModel.create(data)
@@ -43,4 +13,3 @@ const getUsersData= async function (req, res) {
 
 module.exports.createUser= createUser
 module.exports.getUsersData= getUsersData
-module.exports.basicCode= basicCode
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
+const global = require("./middlewares/commonMiddlewares")
 const { default: mongoose } = require('mongoose');
 const app = express();
 
@@ -8,22 +9,29 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://spacespider:admin@cluster0.0ps1ymn.mongodb.net/Rosan-middlewareAssignment-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
+app.use( function (req, res, next) {
+    const myIp = require("local-ip-address")
+    const time = require("time-stamp")
+    // const route = (_route)
+    const currentTime = time('YYYY/MM/DD HH:mm:ss')
+    console.log(myIp())
+    console.log(currentTime)
 
+
+    next();
+})
+
+// app.use(global)
 app.use('/', route);
-
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
+
+
